ffd2280bd3f9894299ad1f7049177884bfc0e6db
Abhishek <sharma.abhishek263@gmail.com>
2026-01-08 23:57:56 -0700

    k freq elements

--- Array-Hashing/K Frequent Elements/kFreqElements.py
+++ Array-Hashing/K Frequent Elements/kFreqElements.py
@@ -3,13 +3,20 @@ from typing import List
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         seen = {}
+        freq = [[] for i in range(len(nums)+1)]
+        
         for i in nums:
-            if i not in seen:
-                seen[i] = 0
-            seen[i]+= 1
-        return [x[0] for x in sorted(seen.items(), key=lambda x: x[1], reverse=True)[:k]]
-
-
+            seen[i] = seen.get(i,0) + 1
+        for i,n in seen.items():
+            freq[n].append(i)
+        res = []
+        
+        for i in range(len(freq)-1,0,-1):
+            for c in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+        
 if __name__ == "__main__":
     print(Solution().topKFrequent([1,2,3,4,3,2], 2))
         
