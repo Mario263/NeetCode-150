55b0df78be58e9ebafbfb69a347f064f3a479e5e
Abhishek <sharma.abhishek263@gmail.com>
2026-01-11 22:26:55 -0700

    encode and decode

--- Array-Hashing/Encode-Decods-Strings/encDecStrings.py
+++ Array-Hashing/Encode-Decods-Strings/encDecStrings.py
@@ -2,17 +2,20 @@ from typing import List
 
 class Solution:
     def encode(self, strs: List[str]) -> str:
-        seen = []
-        for i in strs:
-            seen.append(str(len(i))+"#"+i)
-        return "".join(seen)
-
-    
+        res = ""
+        for s in strs:
+            res+= str(len(s))+"#"+s
+        return res    
     def decode(self, s: str) -> List[str]:
         res, i = [],0
         while i<len(s):
-            j=1
-            
+            j=i
+            while s[j]!="#":
+                j+=1
+            length = int(s[i:j])
+            res.append(s[j+1:j+1+length])
+            i = j+1+length
+        return res
             
         
 if __name__ == "__main__":
