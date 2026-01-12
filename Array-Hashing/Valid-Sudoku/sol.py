5bb0bf0bacc210a3c2fd2356f864c55851c368dc
Abhishek <sharma.abhishek263@gmail.com>
2026-01-12 12:06:38 -0700

    valid sudoku

--- Array-Hashing/Valid-Sudoku/sol.py
+++ Array-Hashing/Valid-Sudoku/sol.py
@@ -2,50 +2,41 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]])-> bool:
-        row = set()
-        col = set()
-        square = set()
+        boxes = [set() for _ in range(9)]
         i = 0
-        # while i < len(board):
-        #     j=0
-        for i in range(len(board)):
+        while i < len(board):
             row_set = set()
-        for j in range(len(board[i])):
-            if board[i][j] != ".":
-                if board[i][j] in row_set:
-                    print(f"Duplicate {board[i][j]} found in row {i}")
-                else:
-                    row_set.add(board[i][j])
-        print(f"Row {i} contents:", row_set)
+            j = 0
+            while j < len(board[i]):
+                cell = board[i][j]
+                if cell != ".":
+                    box_idx = (i // 3) * 3 + (j // 3)
+                    if cell in row_set or cell in boxes[box_idx]:
+                        return False
+                    else:
+                        row_set.add(cell)
+                        boxes[box_idx].add(cell)
+                j += 1
+            i += 1
+    
+        j = 0
+        while j<len(board):
+            col_set = set()
+            i = 0
+            while i< len(board[j]):
+                cell = board[i][j]
+                if cell != ".":
+                    if cell not in col_set:
+                        col_set.add(cell)
+                    else:
+                        return False
+                i+=1
+            j+=1    
+        return True
+    
             
-        #     while j < len(board[i]):   
-        #         if board[i][j] == ".":
-        #             j+=1
-        #         elif board[i][j] not in row:
-        #             row.add(board[i][j])
-        #         j+=1
-        #         if j > len(board[i]):
-        #             break
-        #     i+=1
-        # print(f"Row {i} contents:", row)
-        # j = 0c
-        # while j < len(board):
-        #     i=0
-        #     while i< len(board[j]):
-        #         if board[j][i] == ".":
-        #             i+=1
-        #         elif board[j][i] not in col:
-        #             col.add(board[j][i])
-        #         i+=1
-        #     j+=1
-        # print(f"Column {j} contents:", col)
-        # return row, col
-
-        # return square.add(row,col)
-    
-    
 if __name__ == "__main__":
-    print(Solution().isValidSudoku([
+    print(Solution().isValidSudoku([  
         ["1","2",".",".","3",".",".",".","."],
         ["1",".","2","5",".","4",".",".","."],
         [".","9","1",".",".",".",".",".","3"],
@@ -55,3 +46,4 @@ if __name__ == "__main__":
         [".",".",".",".",".",".","2",".","."],
         [".",".",".","4","1","9",".",".","8"],
         [".",".",".",".","8",".",".","7","9"]]))
+    
