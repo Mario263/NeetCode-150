95bc725b920869ccf0c065ca1793050377d4ffd0
Mario263 <sharma.abhishek263@gmail.com>
2026-01-17 22:18:03 -0700

    3SUm

--- Array-Hashing/Two Sum/twosum.py
+++ Array-Hashing/Two Sum/twosum.py
@@ -7,7 +7,7 @@ class Solution:
             if diff in seen: # checking if the diff is already in the map, if not then we add the current numweber to the map
                 return[seen[diff], i] # if found we return the indices of both the numbers
             seen[x]= i # adding the current number to the map with its index
-        return
+        return List(seen.values()) #type: ignore
     
 if __name__ == "__main__":
     print(Solution().twoSum([2,4,5,6,7,1,3], 9))
---
+++ Two-Pointers/3Sum/sol.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res: List[List[int]] = []
+        n = len(nums)
+
+        for i in range(n):
+            # skip duplicate starting values
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            target = -nums[i]
+            j, k = i + 1, n - 1
+
+            while j < k:
+                curr_sum = nums[j] + nums[k]
+
+                if curr_sum < target:
+                    j += 1
+                elif curr_sum > target:
+                    k -= 1
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+
+                    # skip duplicates for j
+                    j += 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+
+                    # skip duplicates for k
+                    k -= 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+
+        return res
