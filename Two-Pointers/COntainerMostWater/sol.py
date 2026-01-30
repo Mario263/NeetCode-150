8d00a0f09e40fd974e95d36c4cdab2ff2fbb8d34
Abhishek Sharma <sharma.abhishek263@gmail.com>
2026-01-30 00:58:49 -0700

    Contain most water

--- Two-Pointers/COntainerMostWater/sol.py
+++ Two-Pointers/COntainerMostWater/sol.py
@@ -1,15 +1,14 @@
 from typing import List
 
 class Solution:
-    def maxArea(self, heights: List[int]) -> int:
-        l, r = 0, len(heights) - 1
+    def maxArea(self, heights: List[int])-> int:
         res = 0
-
-        while l < r:
-            area = min(heights[l], heights[r]) * (r - l)
+        l,r = 0, len(heights)-1
+        while l<r:
+            area = (r-l) * min(heights[l], heights[r])
             res = max(res, area)
-            if heights[l] <= heights[r]:
-                l += 1
+            if heights[l]<heights[r]:
+                l +=1
             else:
-                r -= 1
-    return
+                r-=1
+        return res
