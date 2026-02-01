d6e11bb8d87f2ea085ebeff314dc4ec1224930ad
Abhishek Sharma <sharma.abhishek263@gmail.com>
2026-01-31 17:57:00 -0700

    Valid Paren

---
+++ Stack/validParenthesis/sol.py
@@ -0,0 +1,9 @@
+class Solution:
+    def validParethesis(slef, s: str)-> bool:
+
+        paren = {")":"(","}":"{","]":"["}
+        for i in paren()
+        return 
+
+if __name__=="__main__":
+    print(Solution().validParethesis())
