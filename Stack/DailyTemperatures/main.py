7eeea716d42dfd8b723016d5d7a92a5c4f798c63
Abhishek Sharma <sharma.abhishek263@gmail.com>
2026-02-07 22:19:56 -0700

    Daily temp

---
+++ Stack/DailyTemperatures/main.py
@@ -0,0 +1,5 @@
+from sol import Solution
+
+
+if __name__=="__main__":
+    print(Solution().dailyTemp([20,42,41,50,21,31,20,60,55]))
--- Stack/DailyTemperatures/sol.py
+++ Stack/DailyTemperatures/sol.py
@@ -14,7 +14,3 @@ class Solution:
         return result 
         
         
-        
-        
-if __name__=="__main__":
-    print(Solution().dailyTemp([20,42,41,50,21,31,20,60,55]))
